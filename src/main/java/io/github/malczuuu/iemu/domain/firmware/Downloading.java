1b00839b91dd428c5fb836b068bd014a179df372
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-03-13 18:54:56 +0100

    Add SslContextFactory

--- src/main/java/io/github/malczuuu/iemu/domain/firmware/Downloading.java
+++ src/main/java/io/github/malczuuu/iemu/domain/firmware/Downloading.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -81,7 +82,7 @@ public class Downloading implements FirmwareUpdateExecution {
 
   private byte[] downloadFile(URI uri) throws Exception {
     if (managedClient) {
-      client = new HttpClient();
+      client = new HttpClient(new SslContextFactory.Client());
       client.start();
     }
     ContentResponse response = client.GET(uri);
